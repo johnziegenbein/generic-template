7b495409f573e7e50471d6e8dbac375395dae6c8
johnziegenbein <johnziegenbein1993@gmail.com>
2021-04-10 22:04:17 +0200

    Add homepage and routing to homepage

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
