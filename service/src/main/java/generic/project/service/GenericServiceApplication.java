625ec6ac40732958ea91aa3034637bae04a36434
johnziegenbein <johnziegenbein1993@gmail.com>
2021-04-10 18:32:57 +0200

    Add gradle and an empty spring boot service

---
+++ service/src/main/java/generic/project/service/GenericServiceApplication.java
@@ -0,0 +1,13 @@
+package generic.project.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenericServiceApplication.class, args);
+	}
+
+}
