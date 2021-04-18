f3f8c01e030ac179a05464c28b9c92836e60fe41
johnziegenbein <johnziegenbein1993@gmail.com>
2021-04-18 17:17:28 +0200

    Add dark theme switch
    Enables switching between light-mode and dark-mode.

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { HomeComponent } from './home/home.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatListModule} from "@angular/material/list";
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,9 +24,11 @@ import {HttpClientModule} from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     NoopAnimationsModule,
     MatToolbarModule,
+    MatSlideToggleModule,
     MatIconModule,
     MatSidenavModule,
     MatListModule,
--- app/src/app/navigation-bar/navigation-bar.component.ts
+++ app/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MODE, ThemeService} from '../theme.service';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent implements OnInit {
+  isDarkMode: boolean;
 
-  constructor() { }
+  constructor(private themeService: ThemeService) {
+    this.themeService.initTheme();
+    this.isDarkMode = this.themeService.isDarMode();
+  }
 
   ngOnInit() {
   }
 
+  toggleDarkMode() {
+    this.isDarkMode = this.themeService.isDarMode();
+
+    this.isDarkMode ? this.themeService.update(MODE.LIGHT_MODE) :
+                      this.themeService.update(MODE.DARK_MODE);
+  }
 }
